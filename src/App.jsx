2218cfdf22b3ce4d49c8551e7c2e0ba6050e735e
vishaldhangar <imvishaldhangar@gmail.com>
2024-04-16 01:48:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+
+    </>
+  )
+}
+
+export default App
